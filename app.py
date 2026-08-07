5e01e4f566f04da029d2c587f6a64ac6334eb5dd
shanymalik21 <shanymalik21@gmail.com>
2026-08-07 19:41:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect, url_flask
+from flask import Flask, request, render_template, jsonify, redirect, url_for,flask
 import sqlite3
 from datetime import datetime
 import re
